9a1b85f36d3b263007a0c072d5b195d9fcac298f
HerbsMagic <136325970+HerbsMagic@users.noreply.github.com>
2024-03-15 15:46:02 +0530

    Some Changes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose")
 const env = require("dotenv").config()
 const MONGO_DB_URI = process.env.MONGO_DB_URI
+const PORT = process.env.PORT
 const server = require("./config/apolloServer")
 mongoose.connect(MONGO_DB_URI).then(() => {
     console.log("Connected to MongoDB")
-    return server.listen({ port: 4000 })
+    return server.listen({ port: PORT })
 }).then(({ url }) => {
     console.log(`Apollo Server ready at ${url}`)
 })
